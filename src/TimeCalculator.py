23544593731c889be420231e8e3f8ac6c98aad3a
landerdr <Lander.DeRoeck@student.uantwerpen.be>
2020-04-22 12:55:41 +0200

    Basic Structure

--- src/TimeCalculator.py
+++ src/TimeCalculator.py
@@ -1,3 +1,18 @@
+import pandas as pd
+import numpy as np
+import re
+
+
 class TimeCalculator:
     def __init__(self):
         pass
+
+    def read_csv(self, input_file: str):
+        df = pd.read_csv(input_file, sep=",")
+
+        print(df["Colour_p4_01"].apply(lambda x: float(x) if re.match(r"\d", x) else 0))
+
+
+if __name__ == "__main__":
+    t = TimeCalculator()
+    t.read_csv("../trainingData/WWWWbbbbbbbbbbWWWW_speed5.csv")
