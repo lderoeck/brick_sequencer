9ed8cfc62b159a27a5eefd7371754454422c14ac
landerdr <lander.deroeck@student.uantwerpen.be>
2020-04-24 12:55:22 +0200

    Dumb proof of concept

--- src/TimeCalculator.py
+++ src/TimeCalculator.py
@@ -10,9 +10,31 @@ class TimeCalculator:
     def read_csv(self, input_file: str):
         df = pd.read_csv(input_file, sep=",")
 
-        print(df["Colour_p4_01"].apply(lambda x: float(x) if re.match(r"\d", x) else 0))
+        arrays = df.transpose().to_numpy()
+
+        vf = np.vectorize(lambda x: x == "6.000" or x == 6)
+
+        for i in range(len(arrays)-1):
+            array = arrays[i]
+            simpl = vf(array)
+            # print(simpl)
+            print(sum(simpl)/8)
+
+        # print(arrays)
+
+        # for i in range(len(arrays)-1):
+        #     array = arrays[i]
+        #     for j in range(len(array)-1):
+
+        # keys = df.keys()
+        # for i in range(1, len(keys)):
+        #     key = keys[i]
+        #     print(df.get(key).apply(lambda x: float(str(x)) if re.match(r"\d", str(x)) else 0))
+
+        # print(df.apply(lambda x: float(x) if re.match(r"\d", x) else 0))
 
 
 if __name__ == "__main__":
     t = TimeCalculator()
     t.read_csv("../trainingData/WWWWbbbbbbbbbbWWWW_speed5.csv")
+    t.read_csv("../trainingData/WWWWggggggggggggWWWW_speed5.csv")
