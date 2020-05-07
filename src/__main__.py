b39d6f1c8217b7f72412fb39503854bb8828430a
pockeleewout <ewout.pockele@student.uantwerpen.be>
2020-05-07 11:39:00 +0200

    Add height option for image export

--- src/__main__.py
+++ src/__main__.py
@@ -26,14 +26,21 @@ def main():
 
     parser = argparse.ArgumentParser(description="A base caller for the Lego brick sequencer")
     parser.add_argument("file", type=str, help="The file to process")
-    parser.add_argument("-i", "--image-file", type=str, required=False, default=None,
+    parser.add_argument("-i", "--image", type=str, required=False, default=None,
                         help="Base name of the generated image files", metavar="IMAGE")
+    parser.add_argument("--height", type=int, required=False, default=16,
+                        help="Height of each sequence in the output image", metavar="HEIGHT")
 
     args = parser.parse_args(sys.argv[1:])
 
     def processing(filename: str, column: str):
         df = pd.read_csv(args.file, index_col=False, usecols=["Time", column], na_values="-")
         df = df.dropna()  # Drop NaN values
+
+        if args.image:
+            from .image import export
+
+            export(df[column], f"{args.image}_{column}_raw.png", args.height)
 
         df = dropNonSequence(df)
 
@@ -42,7 +49,7 @@ def main():
         # Sequence the column and translate the results
         sequence = BaseGuesser.sequence(df, speed)
 
-        if args.image_file:
+        if args.image:
             import math
             from .image import export_multiple
 
@@ -54,7 +61,7 @@ def main():
                 [pd.Series([color] * math.floor(speed * 100), dtype="float32") for color in sequence["Colour"]]
             )
 
-            export_multiple([df[column], speed_sequence, guess_sequence], f"{args.image_file}_{column}.png")
+            export_multiple([df[column], speed_sequence, guess_sequence], f"{args.image}_{column}.png", args.height)
 
         sequence = colour.translate(sequence)
 
